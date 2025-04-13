90804c21c951e00702b0f7cdd808a1ca3698b2d8
soccer47 <stevie.halprin@menloschool.org>
2025-04-12 17:57:24 -0700

    commit

--- src/Connect5.java
+++ src/Connect5.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.HashMap;
+
 public class Connect5 {
     // Instance variables
     // 2D array representing game board
@@ -49,8 +52,11 @@ public class Connect5 {
             // Then return true to get to exit and get to the gameOver screen
             return true;
         }
-        // Switch the turn to the other player
-        isTurnP1 = !isTurnP1;
+        // Otherwise continue with the game
+        else {
+            // Switch the turn to the other player
+            isTurnP1 = !isTurnP1;
+        }
         // Return true to show the move and updating of the board was successful
         return true;
     }
@@ -67,8 +73,13 @@ public class Connect5 {
         // Start by looking diagonally down and left from current spot, then proceed clockwise
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                // If the last move resulted in a streak of 5 in the given direction, return true
-                if (isStreak(lastRow + i, lastCol + j, i, j, 1, validNum)) {
+                // Skip if direction is (0,0) because that’s not a direction
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                // If the last move resulted in a streak of equal to or more than WIN_LENGTH in the given direction
+                // return true
+                if (countStreak(lastRow + i, lastCol + j, i, j, validNum) >= WIN_LENGTH) {
                     return true;
                 }
             }
@@ -80,26 +91,53 @@ public class Connect5 {
     // Helper method for gameWinner
     // Return true if the inputted row of five spaces on the board results all contain the same number
     // Takes in the starting row and col, as well as direction of traversal
-    // Continues until streak of five has been achieved, or until another integer has been hit
-    public boolean isStreak(int row, int col, int rowIncrement, int colIncrement, int spacesVisited, int validNum) {
-        // Base case
-        if (spacesVisited == WIN_LENGTH) {
-            return true;
+    // Continues until streak of five has been achieved, or until another integer has been hit/an index is out of bounds
+    public int countStreak(int row, int col, int rowIncrement, int colIncrement, int validNum) {
+        // The number of current player's pieces in a row
+        // Start at 1 because last clicked index is already included
+        int count = 1;
+        // Continue while index being checked is in bounds and
+        while (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE && board[row][col] == validNum) {
+            // If the index is valid increment count, as well as the row and col being checked
+            count++;
+            row += rowIncrement;
+            col += colIncrement;
         }
-
-        // Make sure the specified spot on the board exists
-        if (row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE) {
-            return false;
-        }
-
-        // Return false if a different integer has been reached
-        if (board[row][col] != validNum) {
-            return false;
-        }
-
-        // Recurse to the next space
-        return isStreak(row + rowIncrement, col + colIncrement, rowIncrement, colIncrement,
-                spacesVisited + 1, validNum);
+        // Return the number of current player's pieces in a row
+        return count;
     }
 
+
+    // Engine method
+    // Return coordinate of best available move for given game scenario
+    public Point getBestMove() {
+        // HashMap to contain coordinates (keys) and their scores (values) of possible moves
+        HashMap<Point, Integer> posMoves = new HashMap<>();
+
+        // Iterate through every available square on the board
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                // Continue to the next index if the current index is unavailable
+                if (board[i][j] != 0) {
+                    continue;
+                }
+
+
+            }
+        }
+
+        // Integer to hold coordinates of highest scored move
+        Point bestMove = (0,0);
+
+        // Iterate through the HashMap of possible moves to find the one with the highest score
+        for (Point placement : posMoves.keySet()) {
+            // Update bestMove if the current possible move is associated with a higher score
+            if (posMoves.get(placement) > posMoves.get(bestMove)) {
+                bestMove = placement;
+            }
+        }
+
+        // Return the best available move
+        return bestMove;
+    }
 }
--- src/Connect5Viewer.java
+++ src/Connect5Viewer.java
@@ -1,24 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Connect5Viewer extends JPanel implements MouseListener, KeyListener {
     enum GameState {
-        START, PLAYER1_TURN, PLAYER2_TURN, PLAYER1_WIN, PLAYER2_WIN, ENGINE_WIN
+        START, PLAYER1_TURN, PLAYER2_TURN, PLAYER1_WIN, PLAYER2_WIN, ENGINE_WIN, DRAW
     }
 
     private JFrame frame;
-    private ImageIcon startingScreen, turnP1, turnP2, winP1, winP2, winEngine;
+    private ImageIcon startingScreen, turnP1, turnP2, winP1, winP2, winEngine, winDraw;
     private GameState state;
     private boolean isSinglePlayer;
     private Connect5 game;
     private final int GRID_SIZE = 5;
     private final int CIRCLE_RADIUS = 30;
     private Point[][] gridPoints = new Point[GRID_SIZE][GRID_SIZE];
+    private AudioPlayer audioPlayer;
+
+    // Pulse effect fields
+    private boolean isPulsing = false;
+    private long pulseStartTime = 0;
+    private Timer pulseTimer;
 
     public Connect5Viewer(boolean isSinglePlayer) {
         this.isSinglePlayer = isSinglePlayer;
-        this.game = null;
+        this.game = new Connect5(isSinglePlayer);
         this.state = GameState.START;
 
         loadImages();
@@ -28,6 +37,9 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
         addKeyListener(this);
         setFocusable(true);
         requestFocusInWindow();
+
+        audioPlayer = new AudioPlayer();
+        playStartingMusic();
     }
 
     private void loadImages() {
@@ -37,6 +49,7 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
         winP1 = new ImageIcon("Resources/winP1.png");
         winP2 = new ImageIcon("Resources/winP2.png");
         winEngine = new ImageIcon("Resources/winEngine.png");
+        winDraw = new ImageIcon("Resources/winDraw.png");
     }
 
     private void setupFrame() {
@@ -65,19 +78,22 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
         }
     }
 
+    private void playStartingMusic() {
+        audioPlayer.playMusic("Resources/startingMusic.wav");
+    }
+
+    private void playGameMusic() {
+        audioPlayer.playMusic("Resources/gameMusic.wav");
+    }
+
+    private void playWinMusic() {
+        audioPlayer.playMusic("Resources/winMusic.wav");
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawBackground(g);
-        if (state != GameState.START) {
-            drawAllGridCircles(g);
-        }
-        if (game != null) {
-            drawPieces(g);
-        }
-    }
 
-    private void drawBackground(Graphics g) {
         switch (state) {
             case START -> g.drawImage(startingScreen.getImage(), 0, 0, getWidth(), getHeight(), null);
             case PLAYER1_TURN -> g.drawImage(turnP1.getImage(), 0, 0, getWidth(), getHeight(), null);
@@ -85,6 +101,12 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
             case PLAYER1_WIN -> g.drawImage(winP1.getImage(), 0, 0, getWidth(), getHeight(), null);
             case PLAYER2_WIN -> g.drawImage(winP2.getImage(), 0, 0, getWidth(), getHeight(), null);
             case ENGINE_WIN -> g.drawImage(winEngine.getImage(), 0, 0, getWidth(), getHeight(), null);
+            case DRAW -> g.drawImage(winDraw.getImage(), 0, 0, getWidth(), getHeight(), null);
+        }
+
+        if (state == GameState.PLAYER1_TURN || state == GameState.PLAYER2_TURN || isPulsing) {
+            drawAllGridCircles(g);
+            drawPieces(g);
         }
     }
 
@@ -100,13 +122,24 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
 
     private void drawPieces(Graphics g) {
         int[][] board = game.board;
+        long elapsed = System.currentTimeMillis() - pulseStartTime;
+
         for (int r = 0; r < GRID_SIZE; r++) {
             for (int c = 0; c < GRID_SIZE; c++) {
                 int val = board[r][c];
                 if (val != 0) {
                     Point p = gridPoints[r][c];
-                    g.setColor(val == 1 ? Color.PINK : Color.CYAN);
-                    g.fillOval(p.x - CIRCLE_RADIUS, p.y - CIRCLE_RADIUS, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2);
+                    boolean isLastPiece = isPulsing && r == game.lastRow && c == game.lastCol;
+
+                    if (isLastPiece) {
+                        double pulseFactor = 1.0 + 0.2 * Math.sin((elapsed / 100.0) * Math.PI);
+                        int radius = (int)(CIRCLE_RADIUS * pulseFactor);
+                        g.setColor(val == 1 ? Color.PINK : Color.CYAN);
+                        g.fillOval(p.x - radius, p.y - radius, radius * 2, radius * 2);
+                    } else {
+                        g.setColor(val == 1 ? Color.PINK : Color.CYAN);
+                        g.fillOval(p.x - CIRCLE_RADIUS, p.y - CIRCLE_RADIUS, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2);
+                    }
                 }
             }
         }
@@ -123,6 +156,7 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
                 game = new Connect5(isSinglePlayer);
                 calculateGridPoints();
                 state = GameState.PLAYER1_TURN;
+                playGameMusic();
                 repaint();
                 return;
             } else if (x >= 630 && x <= 880 && y >= 465 && y <= 540) {
@@ -130,6 +164,7 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
                 game = new Connect5(isSinglePlayer);
                 calculateGridPoints();
                 state = GameState.PLAYER1_TURN;
+                playGameMusic();
                 repaint();
                 return;
             }
@@ -143,18 +178,42 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
                 Point p = gridPoints[r][c];
                 if (Math.hypot(x - p.x, y - p.y) <= CIRCLE_RADIUS) {
                     if (game.board[r][c] == 0) {
+                        boolean currentPlayerIsP1 = game.isTurnP1;
                         boolean moveValid = game.takeTurn(r, c);
+
                         if (moveValid) {
                             if (game.gameOver) {
-                                if (!game.isTurnP1) {
-                                    state = isSinglePlayer ? GameState.PLAYER1_WIN : GameState.PLAYER1_WIN;
+                                if (isBoardFull() && !hasWinner()) {
+                                    state = GameState.DRAW;
+                                    playWinMusic();
                                 } else {
-                                    state = isSinglePlayer ? GameState.ENGINE_WIN : GameState.PLAYER2_WIN;
+                                    isPulsing = true;
+                                    pulseStartTime = System.currentTimeMillis();
+                                    repaint();
+
+                                    pulseTimer = new Timer(50, new ActionListener() {
+                                        @Override
+                                        public void actionPerformed(ActionEvent e) {
+                                            long elapsed = System.currentTimeMillis() - pulseStartTime;
+                                            if (elapsed >= 3000) {
+                                                isPulsing = false;
+                                                pulseTimer.stop();
+                                                if (isSinglePlayer) {
+                                                    state = currentPlayerIsP1 ? GameState.PLAYER1_WIN : GameState.ENGINE_WIN;
+                                                } else {
+                                                    state = currentPlayerIsP1 ? GameState.PLAYER1_WIN : GameState.PLAYER2_WIN;
+                                                }
+                                                playWinMusic();
+                                            }
+                                            repaint();
+                                        }
+                                    });
+                                    pulseTimer.start();
                                 }
                             } else {
                                 state = game.isTurnP1 ? GameState.PLAYER1_TURN : GameState.PLAYER2_TURN;
+                                repaint();
                             }
-                            repaint();
                         }
                     }
                     return;
@@ -163,12 +222,26 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
         }
     }
 
+    private boolean isBoardFull() {
+        for (int r = 0; r < GRID_SIZE; r++) {
+            for (int c = 0; c < GRID_SIZE; c++) {
+                if (game.board[r][c] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasWinner() {
+        return state == GameState.PLAYER1_WIN || state == GameState.PLAYER2_WIN || state == GameState.ENGINE_WIN;
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             game = null;
             state = GameState.START;
             calculateGridPoints();
+            playStartingMusic();
             repaint();
         }
     }
@@ -182,5 +255,28 @@ public class Connect5Viewer extends JPanel implements MouseListener, KeyListener
 
     public static void main(String[] args) {
         new Connect5Viewer(true);
+    }
+
+    // Audio player class to manage background music
+    static class AudioPlayer {
+        private Clip clip;
+
+        public void playMusic(String filePath) {
+            try {
+                if (clip != null && clip.isRunning()) {
+                    clip.stop();
+                    clip.close();
+                }
+
+                File audioFile = new File(filePath);
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                clip = AudioSystem.getClip();
+                clip.open(audioStream);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
